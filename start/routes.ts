e73305704e703e3334bc13dc7efa951beaa0b79d
Pierre Julien <pierrejulienpro@gmail.com>
2022-03-05 13:35:46 +0100

    Création d'une page d'accueil permettant d'afficher des articles

--- start/routes.ts
+++ start/routes.ts
@@ -18,8 +18,6 @@
 |
 */
 
-import Route from '@ioc:Adonis/Core/Route'
+import Route from "@ioc:Adonis/Core/Route";
 
-Route.get('/', async ({ view }) => {
-  return view.render('welcome')
-})
+Route.get("/", "BlogController.index").as("home");
