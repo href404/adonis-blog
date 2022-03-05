0062e6106db94f3fcfddf607362976a2e990c66d
Pierre Julien <pierrejulienpro@gmail.com>
2022-03-05 15:20:49 +0100

    Correction de la migration permettant de créer la table des articles

--- database/migrations/1646484283894_posts.ts
+++ database/migrations/1646484283894_posts.ts
@@ -6,7 +6,7 @@ export default class Posts extends BaseSchema {
   public async up() {
     this.schema.createTable(this.tableName, (table) => {
       table.increments("id");
-      table.string("title").notNullable;
+      table.string("title").notNullable();
       table.text("content", "longtext").nullable();
       table.boolean("online").notNullable().defaultTo(false);
       table.string("thumbnail").nullable();
